e4d4db9e8f9b2df31d56ca4dd5027c5573ec0290
Michael G. Collins <collins.mike@gmail.com>
2011-04-25 22:03:43 +0000

    add CROSSFIRE_BPS to default prefs
    http://code.google.com/p/fbug/source/detail?r=10182

--- firefox/defaults/preferences/defaults.js
+++ firefox/defaults/preferences/defaults.js
@@ -2,4 +2,5 @@ pref("extensions.firebug.DBG_CROSSFIRE", false);
 pref("extensions.firebug.DBG_CROSSFIRE_FRAMES", false);
 pref("extensions.firebug.DBG_CROSSFIRE_TRANSPORT", false);
 pref("extensions.firebug.DBG_CROSSFIRE_TOOLS", false);
+pref("extensions.firebug.DBG_CROSSFIRE_BPS", false);
 pref("extensions.firebug.crossfire.loopbackOnly", true);
